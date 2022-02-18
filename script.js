955ef5e8399a717ac477ee2ece09f7665fa54235
Boni Gattai <bgsiffert@gmail.com>
2022-02-18 16:11:00 -0300

    Updated UI, missing the hover effect on the new generated Canvas.

--- script.js
+++ script.js
@@ -7,30 +7,46 @@ function makeRows(rows, cols) {
         let cell = document.createElement('div');
         container.appendChild(cell).className = 'grid-item';
     }
-}
+};
 
 makeRows(16, 16);
 
 const divs = document.querySelectorAll(".grid-item");
 
-divs.forEach((div) => {
-    div.addEventListener('mouseover', () => {
-        //let randomColor = Math.floor(Math.random()*16777215).toString(16);
-        div.style['background-color'] = 'black';
-    })
-})
+function hoverEffect() {
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', () => {
+            //let randomColor = Math.floor(Math.random()*16777215).toString(16);
+            div.style['background-color'] = 'black';
+        });
+    });
+};
 
+hoverEffect();
 
-const btn = document.querySelector('button');
+const btnRes = document.querySelector('.btnRes');
 
 function restart() {
     divs.forEach((div) => {
-        btn.addEventListener('click', () => {
+        btnRes.addEventListener('click', () => {
             div.style['background-color'] = 'white';
             });
         });
 };
 
+const btnSize = document.querySelector('.btnSize');
 
-btn.addEventListener('click', restart())
+function canvasSize() {
+    btnSize.addEventListener('click', () => {
+        let newGrid = Number(prompt("How big will your next canvas be? (between 4 and 100)"));
+        container.innerHTML = '';
+        makeRows(newGrid, newGrid);
+        if (makeRows()) {
+            hoverEffect();
+        }
+    });
+    
+}
 
+btnRes.addEventListener('click', restart());
+btnSize.addEventListener('click', canvasSize())
